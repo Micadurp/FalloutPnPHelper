064b4ed2f8f0d3939ce40bc11191c8b96ed3a0aa
Micadurp <neoswed@hotmail.com>
2017-05-23 18:21:14 +0200

    Specials AND skills?!?!?! woop!

---
+++ Assets/SpecialsGetter.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpecialsGetter : MonoBehaviour
+{
+    public GameObject mainObject;
+    public string singeltonId;
+
+    public void Start()
+    {
+        if (mainObject.GetComponent<InputField>() != null)
+        {
+            GetValue(mainObject.GetComponent<InputField>());
+        }
+        else if (mainObject.GetComponent<Text>() != null)
+        {
+            GetValue(mainObject.GetComponent<Text>());
+        }
+
+    }
+
+    void GetValue(Text value)
+    {
+        switch (singeltonId)
+        {
+            case "str":
+                value.text = Toolbox.Instance.strength.ToString();
+                break;
+            case "per":
+                value.text = Toolbox.Instance.perception.ToString();
+                break;
+            case "end":
+                value.text = Toolbox.Instance.endurance.ToString();
+                break;
+            case "cha":
+                value.text = Toolbox.Instance.charisma.ToString();
+                break;
+            case "int":
+                value.text = Toolbox.Instance.intelligence.ToString();
+                break;
+            case "agi":
+                value.text = Toolbox.Instance.agility.ToString();
+                break;
+            case "lck":
+                value.text = Toolbox.Instance.luck.ToString();
+                break;
+            default:
+                Debug.LogWarning("singeltonId does not match a singelton");
+                break;
+        }
+    }
+    void GetValue(InputField value)
+    {
+        switch (singeltonId)
+        {
+            case "str":
+                value.text = Toolbox.Instance.strength.ToString();
+                break;
+            case "per":
+                value.text = Toolbox.Instance.perception.ToString();
+                break;
+            case "end":
+                value.text = Toolbox.Instance.endurance.ToString();
+                break;
+            case "cha":
+                value.text = Toolbox.Instance.charisma.ToString();
+                break;
+            case "int":
+                value.text = Toolbox.Instance.intelligence.ToString();
+                break;
+            case "agi":
+                value.text = Toolbox.Instance.agility.ToString();
+                break;
+            case "lck":
+                value.text = Toolbox.Instance.luck.ToString();
+                break;
+            default:
+                Debug.LogWarning("singeltonId does not match a singelton");
+                break;
+        }
+    }
+}
