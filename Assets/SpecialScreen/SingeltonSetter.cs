8cff5cc61f42e0beb7b52627a80346f74cb3cc36
Micadurp <neoswed@hotmail.com>
2017-05-23 17:03:03 +0200

    Added singelton stuff for specials!
    Should be able to access specials ANYWHERE

---
+++ Assets/SpecialScreen/SingeltonSetter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SingeltonSetter : MonoBehaviour
+{
+    public InputField mainInputField;
+    public string singeltonId;
+
+    public void Start()
+    {
+        //Adds a listener that invokes the "LockInput" method when the player finishes editing the main input field.
+        //Passes the main input field into the method when "LockInput" is invoked
+        mainInputField.onEndEdit.AddListener(delegate { SetValue(mainInputField); });
+    }
+
+    void SetValue(InputField input)
+    {
+        switch (singeltonId)
+        {
+            case "str":
+                Toolbox.Instance.strength = int.Parse(input.text);
+                break;
+            case "per":
+                Toolbox.Instance.perception = int.Parse(input.text);
+                break;
+            case "end":
+                Toolbox.Instance.endurance = int.Parse(input.text);
+                break;
+            case "cha":
+                Toolbox.Instance.charisma = int.Parse(input.text);
+                break;
+            case "int":
+                Toolbox.Instance.intelligence = int.Parse(input.text);
+                break;
+            case "agi":
+                Toolbox.Instance.agility = int.Parse(input.text);
+                break;
+            case "lck":
+                Toolbox.Instance.luck = int.Parse(input.text);
+                break;
+            default:
+                Debug.LogWarning("singeltonId does not match a singelton");
+                break;
+        }
+    }
+}
---
+++ Assets/Toolbox.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Toolbox : Singleton<Toolbox>
+{
+    protected Toolbox() { } // guarantee this will be always a singleton only - can't use the constructor!
+
+    public int strength = 1;
+    public int perception = 1;
+    public int endurance = 1;
+    public int charisma = 1;
+    public int intelligence = 1;
+    public int agility = 1;
+    public int luck = 1;
+
+    void Awake()
+    {
+        // Your initialization code here
+    }
+
+    // (optional) allow runtime registration of global objects
+    static public T RegisterComponent<T>() where T : Component
+    {
+        return Instance.GetOrAddComponent<T>();
+    }
+}
